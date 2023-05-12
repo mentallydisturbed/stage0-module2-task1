929813906b7870866c593fdecdea24ec6e3a74a3
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-13 03:01:58 +0600

    qwe

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,7 +2,7 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        
+        //comment
         System.out.println("Hello, World!");
     }
 }
